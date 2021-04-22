95c611e3ce52adf59c71a2c56ea40deac261f004
Utku Halis <20300056@emu.edu.tr>
2021-04-22 13:29:20 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sound-Control
+ This project is a project I created in my development process in 2014.
---
+++ Ses Kontrolü/Form1.vb
@@ -0,0 +1,25 @@
+﻿Option Strict On
+
+Imports System.Runtime.InteropServices
+Public Class Form1
+
+    Private Const APPCOMMAND_VOLUME_MUTE As Integer = &H80000
+    Private Const APPCOMMAND_VOLUME_UP As Integer = &HA0000
+    Private Const APPCOMMAND_VOLUME_DOWN As Integer = &H90000
+    Private Const WM_APPCOMMAND As Integer = &H319
+
+   <DllImport("user32.dll")> Public Shared Function SendMessageW(ByVal hWnd As IntPtr, ByVal Msg As Integer, ByVal wParam As IntPtr, ByVal lParam As IntPtr) As IntPtr
+    End Function
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        SendMessageW(Me.Handle, WM_APPCOMMAND, Me.Handle, New IntPtr(APPCOMMAND_VOLUME_UP))
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        SendMessageW(Me.Handle, WM_APPCOMMAND, Me.Handle, New IntPtr(APPCOMMAND_VOLUME_DOWN))
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        SendMessageW(Me.Handle, WM_APPCOMMAND, Me.Handle, New IntPtr(APPCOMMAND_VOLUME_MUTE))
+    End Sub
+End Class
